df639a29ab30a53ad408cc5b2de105ee7b4eb223
Brickfielder <mion.marco@gmail.com>
2025-11-12 09:50:35 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://brickfielder.github.io',
+  base: '/codex-experiments/',   // <-- repo name with leading & trailing slash
   integrations: [tailwind(), preact()],
   output: 'static',
-  experimental: {
-    contentLayer: false
-  }
+  experimental: { contentLayer: false }
 });
